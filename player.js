f36db0347dcfb0513dfbc376048d3b0bb16217b0
OmerBlechman <37245909+OmerBlechman@users.noreply.github.com>
2018-04-23 14:48:57 +0300

    statistics

--- player.js
+++ player.js
@@ -8,9 +8,7 @@ var player = function () {
     var htmlPlayerDiv = enumCard.dives.PLAYER_CARDS;
 
 
-    function calcCurrentTurn() {
-        currentTurnTime += 1;
-    }
+
     /*function setCards(playerHtml) {
 
         //playerHtml.add(<li>card</li>)
@@ -43,9 +41,21 @@ var player = function () {
         }
     }*/
 
+    function colorPicked(event, pickedColor, playerCard) {
+        event.preventDefault();
+        playerCard.setColor(pickedColor);
+        playerCard.setImage(getUniqueCss(Object.keys(enumCard.enumColor)[pickedColor],
+            Object.keys(enumCard.enumTypes)[enumCard.enumTypes.CHANGE_COLOR],'_'));
+        document.getElementById("pickColor").visibility = "hidden";
+    }
+
     return {
+        calcCurrentTurn: function () {
+                currentTurnTime += 1;
+         },
+
         calculateAVG: function () {
-            averageTimePlayed *= turnsPlayed;
+            averageTimePlayed *= (turnsPlayed - 1);
             averageTimePlayed += currentTurnTime;
             averageTimePlayed /= turnsPlayed;
         },
@@ -54,7 +64,7 @@ var player = function () {
             htmlPlayer.id += Object.keys(enumCard.enumPlayer)[cssID];
         },
 */
-        startClock: function () {
+        resetPlayerClock: function () {
             currentTurnTime = 0;
         },
 
@@ -64,7 +74,7 @@ var player = function () {
 
         setCards: function (theCards) {
             cards = theCards;
-            setInterval(calcCurrentTurn(),1000);
+            setInterval(this.calcCurrentTurn.bind(this),1000);
             for(var i = 0; i < cards.length; ++i){
                 cards[i].setParent(enumCard.dives.PLAYER_CARDS, true);
                 cards[i].changeImage(true);
@@ -88,11 +98,11 @@ var player = function () {
             var promote = card.doOperation(this, lastCard);
             if (takiMode !== undefined) {
                 if(takiModeChecker(cards, takiMode))
-                    promote = 0;
+                    promote = enumCard.enumResult.PLAYER_TURN_AGAIN;
                 else{
                     takiMode = undefined;
-                    if(promote === -1)
-                        promote = 1;
+                    if(promote === enumCard.enumResult.PLAYER_TURN_AGAIN)
+                        promote = enumCard.enumResult.SINGLE;
                 }
             }
             if (cards.length === 1)
@@ -132,44 +142,32 @@ var player = function () {
         isComputer: function () {
             return false;
         },
-
-        selectAndPickColorOperation: function(){
-            var pickedColor = player.pickColor();
-            playerCard.setColor(pickedColor);
-            playerCard.setImage(getUniqueCss(Object.keys(enumCard.enumColor)[pickedColor],
-                Object.keys(enumCard.enumTypes)[enumCard.enumTypes.CHANGE_COLOR],'_'));
-      //      return enumCard.enumResult.PLAYER_TURN_AGAIN;
-        },
       
-        pickColor: function () {
+        pickColor: function (playerCard) {
             var picker;
-            var pickColorId = document.getElementById("pickColor");
+            var pickColorId = document.getElementById(enumCard.dives.PICK_COLOR);
             pickColorId.visibility = "visible";
-            var blue = pickColorId.getElementById("bluePicker");
-            blue.onclick = function (ev) {
-                ev.preventDefault();
-                picker = enumCard.enumColor.BLUE;
-                pickColorId.visibility = "hidden";
-            };
-            var green = pickColorId.getElementById("greenPicker");
-            green.onclick = function (ev) {
-                ev.preventDefault();
-                picker = enumCard.enumColor.GREEN;
-                pickColorId.visibility = "hidden";
-            };
-            var red = pickColorId.getElementById("redPicker");
-            red.onclick = function (ev) {
-                ev.preventDefault();
-                picker = enumCard.enumColor.RED;
-                pickColorId.visibility = "hidden";
-            };
-            var yellow = pickColorId.getElementById("yellowPicker");
-            yellow.onclick = function (ev) {
-                ev.preventDefault();
-                picker = enumCard.enumColor.YELLOW;
-                pickColorId.visibility = "hidden";
-            };
-            //TODO: HTML_OPERATION
+            var blue = pickColorId.getElementById(enumCard.dives.BLUE_PICK);
+            blue.addEventListener("onclick", function (ev) {
+                colorPicked(ev, enumCard.enumColor.BLUE, playerCard);
+            }, false);
+
+            var green = pickColorId.getElementById(enumCard.dives.GREEN_PICK);
+            green.addEventListener("onclick", function (ev) {
+                colorPicked(ev, enumCard.enumColor.GREEN, playerCard);
+            }, false);
+
+            var red = pickColorId.getElementById(enumCard.dives.RED_PICK);
+            red.addEventListener("onclick", function (ev) {
+                colorPicked(ev, enumCard.enumColor.RED, playerCard);
+            }, false);
+
+            var yellow = pickColorId.getElementById(enumCard.dives.YELLOW_PICK);
+            yellow.addEventListener("onclick", function (ev) {
+                colorPicked(ev, enumCard.enumColor.YELLOW, playerCard);
+            }, false);
+
+            return enumCard.enumResult.CONTINUE_PLAYER_TURN;
         },
 
         setTakiMode: function (card) {
--- smartComputer.js
+++ smartComputer.js
@@ -425,6 +425,10 @@ var smartComputer = function() {
             turnsPlayed += 1;
         },
 
+        resetPlayerClock: function(){
+            return 0;
+        },
+
         calculateAVG: function () {
             return 0;
         },
@@ -437,11 +441,11 @@ var smartComputer = function() {
             removeAllCardAppearances(card);
             if (takiMode !== undefined) {
                 if(takiModeChecker(playerCards, takiMode))
-                    promote = 0;
+                    promote = enumCard.enumResult.PLAYER_TURN_AGAIN;
                 else{
                     takiMode = undefined;
-                    if(promote === -1)
-                        promote = 1;
+                    if(promote === enumCard.enumResult.PLAYER_TURN_AGAIN)
+                        promote = enumCard.enumResult.SINGLE;
                 }
             }
             if (playerCards.length === 1)
